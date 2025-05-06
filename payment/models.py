3216751351bbc84769cf78ae7bc8886bb3406486
arman <mdarmanhossen404@gmail.com>
2025-05-06 17:00:11 +0600

    Glamify_backend

--- payment/models.py
+++ payment/models.py
@@ -20,7 +20,7 @@ class Checkout(models.Model):
 
 class Orderitem(models.Model):
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE, blank=True, null=True)
-    # checkout = models.OneToOneField(Checkout, on_delete=models.CASCADE, related_name='items', blank=True, null=True)
+    checkout = models.ForeignKey(Checkout, on_delete=models.CASCADE, related_name='items', blank=True, null=True)
     product_name = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, null=True)
     quantity = models.PositiveIntegerField(default=1) 
     status = models.CharField(max_length=20, choices=[
